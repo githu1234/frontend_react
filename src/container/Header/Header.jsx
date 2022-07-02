e7d8de4d58efee5b036e00bace433345d02f8b72
M.Sathish Kumar Reddy <91124745+githu1234@users.noreply.github.com>
2022-07-02 15:32:44 +0530

    Update Header.jsx
    img profile code deleted

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -41,14 +41,14 @@ const Header = () => {
         transition={{ duration: 5, delayChildren: 0.5 }}
         className="app__header-img"
       >
-        <img src={images.profile} alt="picture" />
-        <motion.img
-          whileInView={{ scale: [0, 1] }}
-          transition={{ duration: 1, ease: 'easeInOut' }}
-          src={images.circle}
-          alt="profile_circle"
-          className="overlay_circle"
-        />
+//         <img src={images.profile} alt="picture" />
+//         <motion.img
+//           whileInView={{ scale: [0, 1] }}
+//           transition={{ duration: 1, ease: 'easeInOut' }}
+//           src={images.circle}
+//           alt="profile_circle"
+//           className="overlay_circle"
+//         />
       </motion.div>
       <motion.div
         variants={scaleVariants}
